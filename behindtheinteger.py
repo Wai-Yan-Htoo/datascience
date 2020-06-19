1dd4f35ae78f2170f80541e947cce18cf8ff06b1
Wai-Yan-Htoo <66287776+Wai-Yan-Htoo@users.noreply.github.com>
2020-06-19 11:35:49 -0700

    Add files via upload

---
+++ behindtheinteger.py
@@ -0,0 +1,16 @@
+#conjugate = give the different forms
+
+
+print(3+1j.conjugate())
+#----------------------------
+
+from fractions import Fraction
+
+a = Fraction(3,2)
+print(a.numerator)
+print(a.denominator)
+
+
+
+#int input() print()  = built-in function
+#conjugate()
